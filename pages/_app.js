9ad92d6104c38f79e85971740173fabbc64f750d
David Z <davidzech27@gmail.com>
2022-09-09 18:56:18 -0700

    Add prettier

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,23 @@ import Head from "next/head"
 import "../styles/globals.css"
 
 const App = ({ Component }) => {
-    return <>
-        <Head>
-            <title>MCHS webdev club</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        
-        <div className="min-h-screen w-screen
+	return (
+		<>
+			<Head>
+				<title>MCHS webdev club</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<div
+				className="min-h-screen w-screen
                       bg-[#050913]
                         relative
                         -z-50"
-        >
-            <Component />
-        </div>
-    </>
+			>
+				<Component />
+			</div>
+		</>
+	)
 }
 
 export default App
--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,37 @@ import { motion } from "framer-motion"
 import FromOffScreen from "/components/FromOffScreen"
 import FadeInText from "/components/FadeInText"
 
-
 const Home = () => {
+	return (
+		<>
+			<FadeInText
+				className="pt-[32vh] mx-auto w-[75%]
+                           text-8xl text-center font-sans font-bold text-[#e4e6e8] z-10"
+			>
+				<h1>Build and deploy impactful websites</h1>
+			</FadeInText>
 
+			<FromOffScreen
+				from="left"
+				className="absolute top-[10vh] left-[-5vw]"
+			>
+				<div
+					className="h-[84vh] w-[10vw]
+                               bg-gradient-to-tr from-[#dd5090] to-[#dd9050] rounded-t-full"
+				></div>
+			</FromOffScreen>
 
-
-    return (
-        <>
-            <FadeInText className="pt-[32vh] mx-auto w-[65%] text-8xl text-center font-sans font-bold text-[#e4e6e8] z-10">
-                <h1>Build and deploy impactful websites.</h1>
-            </FadeInText>
-
-            <FromOffScreen from="left" className="absolute top-[10vh] left-[-5vw]">
-                <div className="h-[84vh] w-[10vw] bg-gradient-to-tr from-[#dd5090] to-[#dd9050] rounded-t-full"></div>
-            </FromOffScreen>
-
-            <FromOffScreen from="right" className="absolute bottom-[6vh] right-0">
-                <div className="h-[84vh] w-[10vw] bg-gradient-to-tl from-[#dd5090] to-[#dd9050] rounded-t-full"></div>
-            </FromOffScreen>
-        </>
-    )
+			<FromOffScreen
+				from="right"
+				className="absolute bottom-[6vh] right-0"
+			>
+				<div
+					className="h-[84vh] w-[10vw]
+                               bg-gradient-to-tl from-[#dd5090] to-[#dd9050] rounded-t-full"
+				></div>
+			</FromOffScreen>
+		</>
+	)
 }
 
 export default Home
