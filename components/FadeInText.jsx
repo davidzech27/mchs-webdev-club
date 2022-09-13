456731b8f2ff193fea97d35b76a06051d765bb1a
David Z <davidzech27@gmail.com>
2022-09-13 08:57:01 -0700

    Tweak text animation

--- components/FadeInText.jsx
+++ components/FadeInText.jsx
@@ -1,26 +1,31 @@
 import { motion } from "framer-motion"
 
 const FadeInText = ({ children, className }) => {
-    return (
-        <motion.div
-            initial={{
-                y: 100,
-                opacity: 0
-            }}
-            animate={{
-                y: 0,
-                opacity: 1
-            }}
-            transition={{
-                type: "tween",
-                duration: 0.8,
-                ease: "easeOut"
-            }}
-            className={className}
-        >
-            {children}
-        </motion.div>
-    )
+	return (
+		<motion.div
+			initial={{
+				y: 100,
+				opacity: 0
+			}}
+			animate={{
+				y: 0,
+				opacity: 1
+			}}
+			transition={{
+				type: "tween",
+				ease: "easeOut",
+				y: {
+					duration: 0.8
+				},
+				opacity: {
+					duration: 1.4
+				}
+			}}
+			className={className}
+		>
+			{children}
+		</motion.div>
+	)
 }
- 
+
 export default FadeInText
