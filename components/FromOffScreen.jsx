b84e21da7ce50c14b77ec936c3c25e7bfeb0c1c9
David Z <davidzech27@gmail.com>
2022-09-19 20:11:18 -0700

    Make responsive

--- components/FromOffScreen.jsx
+++ components/FromOffScreen.jsx
@@ -1,50 +1,24 @@
 import { motion } from "framer-motion"
 
-
-const FromOffScreen = ({ from, children, className }) => {
-    let initial
-    let animate
-
-    if (from === "right") {
-        initial = {
-            x: `${20}vw`,
-            y: `${20 * 3 ** 0.5}vw`,
-            rotate: -30 
-        }
-
-        animate = {
-            x: 0,
-            y: 0,
-            rotate: -30
-        }
-    } else if (from === "left") {
-        initial = {
-            x: `${-20}vw`,
-            y: `${-20 * 3 ** 0.5}vw`,
-            rotate: 150
-        }
-
-        animate = {
-            x: 0,
-            y: 0,
-            rotate: 150
-        }
-    }
-
-    return (
-        <motion.div
-            initial={initial}
-            animate={animate}
-            transition={{
-                type: "spring",
-                stiffness: 100,
-                damping: 12
-            }}
-            className={className}
-        >
-            {children}
-        </motion.div>
-    ) 
+const FromOffScreen = ({ children, className }) => {
+	return (
+		<motion.div
+			initial={{
+				y: "40vw"
+			}}
+			animate={{
+				y: 0
+			}}
+			transition={{
+				type: "spring",
+				stiffness: 100,
+				damping: 12
+			}}
+			className={className}
+		>
+			{children}
+		</motion.div>
+	)
 }
- 
+
 export default FromOffScreen
--- pages/index.js
+++ pages/index.js
@@ -2,113 +2,92 @@ import { motion } from "framer-motion"
 
 import FromOffScreen from "/components/FromOffScreen"
 import FadeInText from "/components/FadeInText"
-// import FloatingButton from "/components/FloatingButton"
+import FloatingButton from "/components/FloatingButton"
 
 const Home = () => {
 	return (
 		<>
 			<FadeInText
 				className="pt-[32vh] mx-auto w-[75%]
-                           text-8xl text-center font-sans font-bold text-[#e4e6e8] z-10"
+                           text-[6.67vw] leading-none text-center font-sans font-bold text-[#e4e6e8] relative z-10"
 			>
 				<h1>Build and deploy impactful websites</h1>
 			</FadeInText>
 
-			<FromOffScreen
-				from="left"
-				className="absolute top-[10vh] left-[-5vw]"
-			>
-				<div
-					className="h-[84vh] w-[10vw]
-                                bg-gradient-to-tr from-[#dd5090] to-[#dd9050] rounded-t-full"
-				></div>
-			</FromOffScreen>
+			<div className="absolute h-[60vw] w-[10vw] min-w-[84px] rotate-[150deg] bottom-[6vh] left-[-7.5vw] ">
+				<FromOffScreen className="absolute">
+					<div
+						className="h-[60vw] w-[10vw] min-w-[84px]
+                               bg-gradient-to-tr from-[#dd5090] to-[#dd9050] rounded-t-full"
+					></div>
+				</FromOffScreen>
+				<svg
+					className="absolute top-[7vw] left-[-5.5vw]"
+					width="7vw"
+					height="698"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M84.4482 1.48117C85.9994 -1.14999 90.0304 -0.0495114 90.0304 3.00513L90.0304 145.012C90.0304 145.03 90.0302 145.047 90.03 145.064L90 697.017L0.0303943 697.013L0.0602304 147.012C0.0602304 146.229 0.15044 145.466 0.320915 144.734C0.354961 144.37 0.458167 144.003 0.639747 143.65C1.06609 142.455 1.71308 141.366 2.53162 140.43L84.4482 1.48117Z"
+						fill="#E4E6E8"
+						fill-opacity="0.06"
+					/>
+				</svg>
 
-			<FromOffScreen
-				from="right"
-				className="absolute bottom-[6vh] right-0"
-			>
-				<div
-					className="h-[84vh] w-[10vw]
+				<svg
+					className="absolute top-[-29px] right-[-4vw]"
+					width="7vw"
+					height="698"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M84.4482 1.48117C85.9994 -1.14999 90.0304 -0.0495114 90.0304 3.00513L90.0304 145.012C90.0304 145.03 90.0302 145.047 90.03 145.064L90 697.017L0.0303943 697.013L0.0602304 147.012C0.0602304 146.229 0.15044 145.466 0.320915 144.734C0.354961 144.37 0.458167 144.003 0.639747 143.65C1.06609 142.455 1.71308 141.366 2.53162 140.43L84.4482 1.48117Z"
+						fill="#E4E6E8"
+						fill-opacity="0.06"
+					/>
+				</svg>
+			</div>
+
+			<div className="absolute h-[60vw] w-[10vw] min-w-[84px] rotate-[-30deg] top-[6.5vh] right-[-3vw]">
+				<FromOffScreen className="absolute">
+					<div
+						className="h-[60vw] w-[10vw] min-w-[84px]
                                bg-gradient-to-tl from-[#dd5090] to-[#dd9050] rounded-t-full"
-				></div>
-			</FromOffScreen>
+					></div>
+				</FromOffScreen>
 
-			<svg
-				className="absolute top-0 left-[-3.2%]"
-				width="302"
-				height="615"
-				viewBox="0 0 302 615"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					d="M227.604 610.575C227.576 613.63 223.533 614.693 222.006 612.047L151.002 489.065C150.994 489.051 150.985 489.036 150.977 489.02L-124.974 11L-47.0289 -33.9958L227.945 442.333C228.337 443.012 228.64 443.717 228.858 444.436C229.011 444.769 229.105 445.138 229.125 445.535C229.352 446.783 229.337 448.05 229.095 449.27L227.604 610.575Z"
-					fill="#E4E6E8"
-					fillOpacity="0.05"
-				/>
-			</svg>
+				<svg
+					className="absolute top-[2.5vw] right-[-4vw]"
+					width="7vw"
+					height="698"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M84.4482 1.48117C85.9994 -1.14999 90.0304 -0.0495114 90.0304 3.00513L90.0304 145.012C90.0304 145.03 90.0302 145.047 90.03 145.064L90 697.017L0.0303943 697.013L0.0602304 147.012C0.0602304 146.229 0.15044 145.466 0.320915 144.734C0.354961 144.37 0.458167 144.003 0.639747 143.65C1.06609 142.455 1.71308 141.366 2.53162 140.43L84.4482 1.48117Z"
+						fill="#E4E6E8"
+						fill-opacity="0.06"
+					/>
+				</svg>
 
-			<svg
-				className="absolute top-[200px] -left-[10.5%]"
-				width="302"
-				height="615"
-				viewBox="0 0 302 615"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					d="M227.604 610.575C227.576 613.63 223.533 614.693 222.006 612.047L151.002 489.065C150.994 489.051 150.985 489.036 150.977 489.02L-124.974 11L-47.0289 -33.9958L227.945 442.333C228.337 443.012 228.64 443.717 228.858 444.436C229.011 444.769 229.105 445.138 229.125 445.535C229.352 446.783 229.337 448.05 229.095 449.27L227.604 610.575Z"
-					fill="#E4E6E8"
-					fillOpacity="0.05"
-				/>
-			</svg>
-			<svg
-				className="absolute bottom-[14%] right-[-7%] rotate-180"
-				width="302"
-				height="615"
-				viewBox="0 0 302 615"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					d="M227.604 610.575C227.576 613.63 223.533 614.693 222.006 612.047L151.002 489.065C150.994 489.051 150.985 489.036 150.977 489.02L-124.974 11L-47.0289 -33.9958L227.945 442.333C228.337 443.012 228.64 443.717 228.858 444.436C229.011 444.769 229.105 445.138 229.125 445.535C229.352 446.783 229.337 448.05 229.095 449.27L227.604 610.575Z"
-					fill="#E4E6E8"
-					fillOpacity="0.05"
-				/>
-			</svg>
+				<svg
+					className="absolute top-[10vw] left-[-4vw]"
+					width="7vw"
+					height="698"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M84.4482 1.48117C85.9994 -1.14999 90.0304 -0.0495114 90.0304 3.00513L90.0304 145.012C90.0304 145.03 90.0302 145.047 90.03 145.064L90 697.017L0.0303943 697.013L0.0602304 147.012C0.0602304 146.229 0.15044 145.466 0.320915 144.734C0.354961 144.37 0.458167 144.003 0.639747 143.65C1.06609 142.455 1.71308 141.366 2.53162 140.43L84.4482 1.48117Z"
+						fill="#E4E6E8"
+						fill-opacity="0.06"
+					/>
+				</svg>
+			</div>
 
-			<svg
-				className="absolute bottom-[-22%] right-[-7%] rotate-180"
-				width="302"
-				height="615"
-				viewBox="0 0 302 615"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					d="M227.604 610.575C227.576 613.63 223.533 614.693 222.006 612.047L151.002 489.065C150.994 489.051 150.985 489.036 150.977 489.02L-124.974 11L-47.0289 -33.9958L227.945 442.333C228.337 443.012 228.64 443.717 228.858 444.436C229.011 444.769 229.105 445.138 229.125 445.535C229.352 446.783 229.337 448.05 229.095 449.27L227.604 610.575Z"
-					fill="#E4E6E8"
-					fillOpacity="0.05"
-				/>
-			</svg>
-
-			{/* <svg
-				className="absolute bottom-[-30%] right-[-1%] rotate-180"
-				width="302"
-				height="615"
-				viewBox="0 0 302 615"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					d="M227.604 610.575C227.576 613.63 223.533 614.693 222.006 612.047L151.002 489.065C150.994 489.051 150.985 489.036 150.977 489.02L-124.974 11L-47.0289 -33.9958L227.945 442.333C228.337 443.012 228.64 443.717 228.858 444.436C229.011 444.769 229.105 445.138 229.125 445.535C229.352 446.783 229.337 448.05 229.095 449.27L227.604 610.575Z"
-					fill="#E4E6E8"
-					fillOpacity="0.05"
-				/>
-			</svg> */}
-
-			{/* <FloatingButton></FloatingButton> */}
+			<FloatingButton />
 		</>
 	)
 }
